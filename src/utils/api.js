3c8a8da72f9fc656c52178d2f05966500fb96be7
Matt <matteopieroni6@gmail.com>
2019-06-01 17:02:22 +0100

    Fixed Axios handling

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,16 +8,16 @@ import { urls, i18n } from "../constants";
 
 export const fetchMovies = async () => {
   try {
-    const movies = axios(
+    const { data } = await axios(
       `${urls.nowPlaying}?api_key=${process.env.TMDB_API_KEY}&language=${
         i18n.language
       }&page=1&region=${i18n.region}`
     );
-    // TODO add error handling
-    if (!movies || !movies.results)
-      throw new Error("We couldn't find any movies");
 
-    return movies;
+    // TODO add error handling
+    if (!data || !data.results) throw new Error("We couldn't find any movies");
+
+    return data;
   } catch (e) {
     throw new Error("We couldn't find any movies");
   }
